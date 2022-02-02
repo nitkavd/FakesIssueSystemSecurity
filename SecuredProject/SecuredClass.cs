456f691b30d09021d5426f0d5b6241daef2d6350
Koen Van Damme <koen.vandamme@net-it.be>
2022-02-02 10:06:40 +0100

    Add project files.

---
+++ SecuredProject/SecuredClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+[assembly: System.Security.SecurityRules(global::System.Security.SecurityRuleSet.Level2, SkipVerificationInFullTrust = true)]
+
+namespace SecuredProject
+{
+    public class SecuredClass
+    {
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ SecuredProjectTests/SecuredClassTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecuredProject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecuredProject.Tests
+{
+    [TestClass()]
+    public class SecuredClassTests
+    {
+        [TestMethod()]
+        public void HelloWorldTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
